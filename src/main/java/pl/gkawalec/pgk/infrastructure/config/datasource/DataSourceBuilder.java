20097d9a531cdf1cb5220fa7016b4b7a9fff3dea
Grzegorz Kawalec <gkawalec4@gmail.com>
2021-04-20 19:59:54 +0200

    #1 Inicjalizacja backendu -> P6Spy

---
+++ src/main/java/pl/gkawalec/pgk/infrastructure/config/datasource/DataSourceBuilder.java
@@ -0,0 +1,38 @@
+package pl.gkawalec.pgk.infrastructure.config.datasource;
+
+import com.p6spy.engine.spy.P6SpyDriver;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+@RequiredArgsConstructor
+class DataSourceBuilder {
+
+    private final DataSourceProperties dataSourceProperties;
+
+    private static final String P6SPY_IN_URL = "p6spy:";
+
+    DataSource build() {
+        HikariDataSource dataSource = (HikariDataSource) dataSourceProperties.initializeDataSourceBuilder().build();
+        dataSource.setMaximumPoolSize(10);
+        addP6Spy(dataSource);
+        return dataSource;
+    }
+
+    private void addP6Spy(HikariDataSource dataSource) {
+        String jdbcUrl = dataSource.getJdbcUrl();
+        if (jdbcUrl.contains(P6SPY_IN_URL)) {
+            jdbcUrl = jdbcUrl.replace(P6SPY_IN_URL, "");
+            dataSource.setJdbcUrl(jdbcUrl);
+        }
+        if (jdbcUrl.contains(P6SPY_IN_URL)) {
+            String name = P6SpyDriver.class.getName();
+            dataSource.setDriverClassName(name);
+        }
+    }
+
+}
