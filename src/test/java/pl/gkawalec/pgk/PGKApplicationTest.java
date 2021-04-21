c2b64b91d143a7dae59ec40148d9df926e8b554f
Grzegorz Kawalec <gkawalec4@gmail.com>
2021-04-21 19:21:26 +0200

    #1 Inicjalizacja backendu -> Konfiguracja testów automatycznych oraz CI w GitHub i Heroku

---
+++ src/test/java/pl/gkawalec/pgk/PGKApplicationTest.java
@@ -0,0 +1,15 @@
+package pl.gkawalec.pgk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PGKApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
