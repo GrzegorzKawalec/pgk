89d48e1e8c1ba75ceb1c6108b08baf1a7d3b2a0a
Grzegorz Kawalec <gkawalec4@gmail.com>
2021-04-24 15:50:12 +0200

    Refaktoryzacja -> zmiana nazwy klasy

--- src/main/java/pl/gkawalec/pgk/application/info/InfoService.java
+++ src/main/java/pl/gkawalec/pgk/application/info/InfoService.java
@@ -3,7 +3,7 @@ package pl.gkawalec.pgk.application.info;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.gkawalec.pgk.infrastructure.setting.model.AppSetting;
-import pl.gkawalec.pgk.web.dto.info.InfoBasicDto;
+import pl.gkawalec.pgk.web.dto.info.InfoBasicDTO;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +11,8 @@ public class InfoService {
 
     private final AppSetting appSetting;
 
-    public InfoBasicDto getBasicInfo() {
-        return InfoBasicDto.of(appSetting);
+    public InfoBasicDTO getBasicInfo() {
+        return InfoBasicDTO.of(appSetting);
     }
 
 }
