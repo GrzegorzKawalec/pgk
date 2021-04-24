c4ccf77d46d0dcef1223755445c9bd20efd9e14b
Grzegorz Kawalec <gkawalec4@gmail.com>
2021-04-24 14:38:02 +0200

    #3 Inicjalizacja frontendu -> Angular

---
+++ src/main/webapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pgk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+}
