a975e0dec7d643c581ec9c21b129615f7461e054
Grzegorz Kawalec <gkawalec4@gmail.com>
2021-04-24 13:22:43 +0200

    #1 Inicjalizacja backendu -> Poprawiono profile aplikacji

--- src/test/java/pl/gkawalec/pgk/testconfig/annotation/PGKTestProfiles.java
+++ src/test/java/pl/gkawalec/pgk/testconfig/annotation/PGKTestProfiles.java
@@ -1,15 +1,14 @@
 package pl.gkawalec.pgk.testconfig.annotation;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@SpringBootTest
-@PGKTestProfiles
+@ActiveProfiles("test_a")
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface PGKSpringBootTest {
+public @interface PGKTestProfiles {
 }
