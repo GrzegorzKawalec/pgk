9bbaae34fb41752713bbdf2fff7493ac835fdf0f
Grzegorz Kawalec <gkawalec4@gmail.com>
2021-04-24 17:13:06 +0200

    #3 Inicjalizacja frontendu -> Generator DTO z backendu na frontend

---
+++ src/main/webapp/src/app/api/api-models.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+// Generated using typescript-generator version 2.30.840 on 2021-04-24 17:00:47.
+
+export interface InfoBasicDTO {
+    name?: string;
+    version?: string;
+    author?: string;
+}
