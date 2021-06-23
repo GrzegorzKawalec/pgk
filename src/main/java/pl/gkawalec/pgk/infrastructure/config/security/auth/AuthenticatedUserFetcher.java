d1bd47421bfe2203fefdd4131c7150149f1932ec
Grzegorz Kawalec <gkawalec4@gmail.com>
2021-06-23 20:34:42 +0200

    Poprawiono klasę AuthenticatedUserFetcher

--- src/main/java/pl/gkawalec/pgk/infrastructure/config/security/auth/AuthenticatedUserFetcher.java
+++ src/main/java/pl/gkawalec/pgk/infrastructure/config/security/auth/AuthenticatedUserFetcher.java
@@ -2,6 +2,7 @@ package pl.gkawalec.pgk.infrastructure.config.security.auth;
 
 import lombok.experimental.UtilityClass;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 
@@ -11,15 +12,20 @@ import java.util.Objects;
 class AuthenticatedUserFetcher {
 
     User getAuthenticatedUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (isValid(authentication)) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        if (Objects.isNull(context)) {
+            return null;
+        }
+        Authentication authentication = context.getAuthentication();
+        if (isInvalid(authentication)) {
             return null;
         }
         return (User) authentication.getPrincipal();
     }
 
-    private boolean isValid(Authentication authentication) {
-        return !authentication.isAuthenticated() ||
+    private boolean isInvalid(Authentication authentication) {
+        return Objects.isNull(authentication) ||
+                !authentication.isAuthenticated() ||
                 Objects.isNull(authentication.getPrincipal()) ||
                 !(authentication.getPrincipal() instanceof User);
     }
